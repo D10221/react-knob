d0447ff43a42afcbc4db543dc2e13e0ce73f0529
Mr D <d10221@protonmail.com>
2020-01-13 14:27:16 +0200

    Do not export Rotate (internal)

--- packages/react-knob/src/index.tsx
+++ packages/react-knob/src/index.tsx
@@ -1,5 +1,4 @@
 export { default } from "./Knob";
 export { default as CssSkin } from "./KnobSkin";
 export { default as SvgSkin } from "./KnobSkinSvg";
-export { default as Rotate } from "./rotate";
 export { default as SimpleSkin}  from "./KnobSkinSvgSimple";
