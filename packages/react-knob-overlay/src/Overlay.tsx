665b137cf91e3aed9cbfb992a487c889d557ed4c
Daniel <danieliozzi@gmail.com>
2020-01-17 04:39:15 +0200

    overlay: basehight as parameter

--- packages/react-knob-overlay/src/Overlay.tsx
+++ packages/react-knob-overlay/src/Overlay.tsx
@@ -1,57 +1,68 @@
 import React, { FunctionComponent } from "react";
+import Styles from "./styles";
 /** */
-const BASE_HEIGHT = 100;
-/** */
-type KnobOverlayProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
-    topPosition: number;
-    scale: number;
-    knobCenter: number[];
-    cursorPos: number[];
-    overlayColor?: string | undefined;
-    className?: string | undefined,
-    lineClassName?: string | undefined,
-    lineStyle?: React.CSSProperties | undefined
+type KnobOverlayProps = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+> & {
+  topPosition: number;
+  scale: number;
+  knobCenter: number[];
+  cursorPos: number[];
+  overlayColor?: string | undefined;
+  className?: string | undefined;
+  lineClassName?: string | undefined;
+  lineStyle?: React.CSSProperties | undefined;
+  baseHight?: number | undefined;
 };
 /** */
 const cartesian2Polar: (
-    x: number[],
-    y: number[],
+  x: number[],
+  y: number[],
 ) => { distance: number; degrees: number } =
-    /** */
-    ([x1, y1], [x2, y2]) => {
-        const x = x2 - x1;
-        const y = y2 - y1;
-        const distance = Math.sqrt(x * x + y * y);
-        const radians = Math.atan2(y, x);
-        const degrees = radians * (180 / Math.PI);
-        return { distance, degrees };
-    };
-import Styles from "./styles";
+  /** */
+  ([x1, y1], [x2, y2]) => {
+    const x = x2 - x1;
+    const y = y2 - y1;
+    const distance = Math.sqrt(x * x + y * y);
+    const radians = Math.atan2(y, x);
+    const degrees = radians * (180 / Math.PI);
+    return { distance, degrees };
+  };
 /** */
 const KnobOverlay: FunctionComponent<KnobOverlayProps> = ({
-    topPosition = 0,
-    scale = 1,
-    knobCenter = [],
-    cursorPos = [],
-    className,
-    lineClassName,
+  topPosition = 0,
+  scale = 1,
+  knobCenter = [],
+  cursorPos = [],
+  className,
+  lineClassName,
+  style,
+  lineStyle,
+  baseHight = 100,
+  ...props
+}) => {
+  if (!topPosition) return null;
+  const { distance, degrees } = cartesian2Polar(knobCenter, cursorPos);
+  const verticalLineScale = baseHight * scale;
+  const styles = Styles({
     style,
     lineStyle,
-    ...props
-}) => {
-    if (!topPosition) return null;
-    const { distance, degrees } = cartesian2Polar(knobCenter, cursorPos);
-    const verticalLineScale = BASE_HEIGHT * scale;
-    const styles = Styles({ style, lineStyle, distance, degrees, cursorPos, topPosition, verticalLineScale, knobCenter });
-    return (
-        <div className={className} style={styles.overlay} {...props} >
-            <div className={lineClassName} style={styles.knobPath}></div>
-            <div className={lineClassName} style={styles.bodyPath}></div>
-            <div className={lineClassName} style={styles.topPath}></div>
-            <div className={lineClassName} style={styles.centerPath}></div>
-            <div className={lineClassName} style={styles.bottomPath}></div>
-        </div>
-    );
+    distance,
+    degrees,
+    cursorPos,
+    topPosition,
+    verticalLineScale,
+    knobCenter,
+  });
+  return (
+    <div className={className} style={styles.overlay} {...props}>
+      <div className={lineClassName} style={styles.knobPath}></div>
+      <div className={lineClassName} style={styles.bodyPath}></div>
+      <div className={lineClassName} style={styles.topPath}></div>
+      <div className={lineClassName} style={styles.centerPath}></div>
+      <div className={lineClassName} style={styles.bottomPath}></div>
+    </div>
+  );
 };
-
 export default KnobOverlay;
